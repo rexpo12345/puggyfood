884b70550ff525c5226c19a91170740752964e94
rexpo12345 <83203028+rexpo12345@users.noreply.github.com>
2022-08-16 14:35:22 +0800

    Update logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,8 +1,7 @@
 function logoutMe() {
-
     sessionStorage.removeItem("token");
     sessionStorage.removeItem("userName");
     sessionStorage.removeItem("_id");
     sessionStorage.removeItem("userPassword");
-    window. location. replace("http://127.0.0.1:8080");
+    window. location. replace("http://ec2-34-227-58-112.compute-1.amazonaws.com:8080");
 }
