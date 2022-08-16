36ed29e29cbdea51432ab028eb57b92be9dee308
rexpo12345 <83203028+rexpo12345@users.noreply.github.com>
2022-08-16 22:40:15 +0800

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ function loginMe(){
         console.log(token.result);
         
             if (token.result != "invalid") {
-                window. location. replace("http://ec2-34-227-58-112.compute-1.amazonaws.com:8080/userProfile.html");
+                window. location. replace("http://ec2-44-208-133-128.compute-1.amazonaws.com/userProfile.html");
                 sessionStorage.setItem("token", token.result);
                 sessionStorage.setItem("userName", userName);
                 sessionStorage.setItem("userPassword", userPassword);
