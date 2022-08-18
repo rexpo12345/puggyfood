d25233f7ac43c12f74cf818a50b6b6ddb67b3f1b
rexpo12345 <83203028+rexpo12345@users.noreply.github.com>
2022-08-18 19:25:13 +0800

    Create polly.js

---
+++ public/js/polly.js
@@ -0,0 +1,28 @@
+(function() {
+  const elemPlayButton = document.querySelector("#play-button")
+  const elemVoice = document.querySelector("#transaltion");
+  const elemText = document.getElementById("aboutus");
+
+  elemPlayButton.addEventListener("click", function() {
+    if (elemVoice.value == "english") {
+      var voice = "Matthew"
+    }
+
+    if (elemVoice.value == "chinese") {
+      var voice = "Zhiyu"
+    }
+
+    let url = "https://h3qb54enf9.execute-api.us-east-1.amazonaws.com/default/polly"
+    url += "?voice=" + encodeURIComponent(voice);
+    url += "&text=" + encodeURIComponent(elemText.textContent);
+
+    const elemAudio = document.createElement("AUDIO");
+    elemAudio.style.display = "none";
+    document.body.appendChild(elemAudio);
+    elemAudio.controls = true;
+    elemAudio.src = url;
+    elemAudio.play();
+
+  });
+
+}());
